fe20b11cd64d8cb458a1d90689c556781f53347c
Kyle Wills <32134945+kylewills@users.noreply.github.com>
2022-02-18 17:14:25 +0000

    Add files via upload

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+
+
+const data ={
+    bikeproducts:[{
+        _id: 1,
+        name: 'Red Mountain Bike',
+        category: 'Mountain Bikes',
+        image: '/images/p1.jpg',
+        price: 1500,
+        brand: 'Canyon',
+        rating: 4.8,
+        numReviews: 10
+    },
+    {
+        _id: 2,
+        name: 'Blue Mountain Bike',
+        category: 'Mountain Bikes',
+        image: '/images/p1.jpg',
+        price: 1350,
+        brand: 'Canyon',
+        rating: 4.7,
+        numReviews: 18
+    },
+    {
+        _id: 3,
+        name: 'Black Mountain Bike',
+        category: 'Mountain Bikes',
+        image: '/images/p1.jpg',
+        price: 1400,
+        brand: 'Canyon',
+        rating: 4.3,
+        numReviews: 17
+    }]
+}
+
+export default data; 
