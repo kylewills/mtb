278bbe4c26ab024f89df3e5ad9de91b377082acd
Kyle <[Kyledwills@virginmedia.com]>
2022-02-18 16:01:54 +0000

    added axios and redux

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+// import data from '../data';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+// import { useDispatch, useSelector } from 'react-redux';
+// import{listProducts} from '../actions/productActions';
 
 function HomePage (props) {
-    return <ul className="products">
+
+   const [bikeproducts, setProduct] = useState([]);
+  // const productList = useSelector(state => state.productList);
+  // const { products, loading, error } = productList;
+  // const dispatch = useDispatch();
+
+  useEffect(() =>{
+    // dispatch(listProducts());
+   const fetchData = async () => {
+    const {data} = await axios.get("/api/products"); 
+   setProduct(data);
+   }
+    fetchData()
+    return () => {
+      
+    };
+  }, [])
+
+    return ( 
+      // loading? <div>Loading...</div>:
+      // error? <div>{error}</div>:
+
+      <ul className="products">
     {
-      data.bikeproducts.map(product =>
-        <li>
+      bikeproducts.map(product =>
+        // data.bikeproducts.map(product =>
+
+        <li key={product._id}>
       <div className="product">
       <Link to={'/product/' + product._id}><img className="product-image" src= {product.image} alt="product" /></Link>
        
@@ -20,7 +47,7 @@ function HomePage (props) {
     </li>)
     }
   </ul>
-
+  );
 }
 
 export default HomePage;
